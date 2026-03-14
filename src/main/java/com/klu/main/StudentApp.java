fe7d99a9fcd2750c3f9ffda9d1f096f0f1b0f260
Manjusha <manjushatallada@gmail.com>
2026-03-14 21:41:44 +0530

    Added Skill Experiment-4

---
+++ src/main/java/com/klu/main/StudentApp.java
@@ -0,0 +1,16 @@
+package com.klu.main;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.klu.Student;
+
+public class StudentApp {
+
+	public static void main(String[] args) {
+		   ApplicationContext context =
+		                new ClassPathXmlApplicationContext("applicationContext.xml");
+		  Student student = context.getBean("student",Student.class);
+		        student.display();
+		    }
+
+}
